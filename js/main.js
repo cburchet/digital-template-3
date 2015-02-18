ab3a142d59e0971e19ecf1d98435d50b302448ee
cburchet <cburchet@gmu.edu>
2015-02-18 17:53:19 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -138,6 +138,7 @@ window.onload = function()
 				boulder.rotation = game.rnd.integerInRange(1, 5);
 				//  Let gravity do its thing
 				boulder.body.gravity.y = 15;
+				boulder.lifespan = 5000;
 			}
 		}
 	}
